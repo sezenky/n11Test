6cbfe767a390963c9620e2596ca6168819049bc8
sezenky <sezenky@gmail.com>
2019-05-31 07:08:40 +0300

    Update search.py

--- search.py
+++ search.py
@@ -7,13 +7,10 @@ class UserBehavior(TaskSet):
     @task(1)
     def create_post(self):
         headers = {'content-type': 'application/json', 'Accept-Encoding': 'gzip'}
-        self.client.post("/arama?q=iphone", data=json.dumps({
-            "userId": 1
-        }),
+        self.client.post("/arama?q=iphone",
                          headers=headers,
-                         name="Create a new post")
+                         name="Search for iPhone")
 
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-
